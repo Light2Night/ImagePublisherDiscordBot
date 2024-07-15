389f11bbd25e45dfae494e12b375b74799cf55a2
Приходько Владислав <3885vlad@gmail.com>
2024-07-15 19:51:34 +0300

    Added AppConfigurations loading

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -4,17 +4,38 @@ using Newtonsoft.Json;
 string programDir = Directory.GetCurrentDirectory();
 string dataDir = Path.Combine(programDir, "DataAndConfigurations");
 string configurationsPath = Path.Combine(dataDir, "appsettings.json");
+string logPath = Path.Combine(dataDir, "log.txt");
 
 var dataDirectory = new DirectoryInfo(dataDir);
-if (!dataDirectory.Exists)
+if (!dataDirectory.Exists) {
 	dataDirectory.Create();
+}
+
+var logger = new SimpleLogger(logPath);
+logger.Info("The program has been launched");
 
 if (!File.Exists(configurationsPath)) {
 	var defaultSettings = new AppConfigurations("", 10, "Images", true);
 	var json = JsonConvert.SerializeObject(defaultSettings);
 	File.WriteAllText(configurationsPath, json);
 
-	// Log
+	logger.Info("Created the default appsettings.json in the DataAndConfigurations directory. You must initialize this file and rerun the program");
 
 	return;
 }
+
+AppConfigurations configurations;
+
+try {
+	var json = File.ReadAllText(configurationsPath);
+
+	configurations = JsonConvert.DeserializeObject<AppConfigurations>(json)
+		?? throw new NullReferenceException("JsonConvert.DeserializeObject returns null");
+}
+catch (NullReferenceException e) {
+	logger.Error(e.Message);
+}
+catch (JsonSerializationException e) {
+	logger.Error($"appsettings.json is invalid: {e.Message}");
+}
+
---
+++ Bot/SimpleLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace Bot;
+
+public class SimpleLogger(string filePath) {
+
+	public void Info(string message) => LogText("Info", message);
+	public void Log(string message) => LogText("Log", message);
+	public void Warning(string message) => LogText("Warning", message);
+	public void Error(string message) => LogText("Error", message);
+	public void Critical(string message) => LogText("Critical", message);
+
+	private void LogText(string logType, string text) {
+		string log = $"{logType,-8} | {DateTime.Now,-10} | {text}\n";
+
+		Console.Write(log);
+		File.AppendAllText(filePath, log);
+	}
+}
