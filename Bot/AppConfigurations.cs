e200b3b9c7fd4faac7512b9d106bf1a9c9dd94f4
Приходько Владислав <3885vlad@gmail.com>
2024-07-15 19:27:46 +0300

    appsettings.json will be created if it doesn't exist

---
+++ Bot/AppConfigurations.cs
@@ -0,0 +1,14 @@
+﻿namespace Bot;
+
+public class AppConfigurations(
+	string botToken,
+	int publicationIntervalSeconds,
+	string imagesDirName,
+	bool publishOnLaunch
+) {
+
+	public readonly string BotToken = botToken;
+	public readonly int PublicationIntervalSeconds = publicationIntervalSeconds;
+	public readonly string ImagesDirName = imagesDirName;
+	public readonly bool PublishOnLaunch = publishOnLaunch;
+}
--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Bot;
+using Newtonsoft.Json;
+
+string programDir = Directory.GetCurrentDirectory();
+string dataDir = Path.Combine(programDir, "DataAndConfigurations");
+string configurationsPath = Path.Combine(dataDir, "appsettings.json");
+
+var dataDirectory = new DirectoryInfo(dataDir);
+if (!dataDirectory.Exists)
+	dataDirectory.Create();
+
+if (!File.Exists(configurationsPath)) {
+	var defaultSettings = new AppConfigurations("", 10, "Images", true);
+	var json = JsonConvert.SerializeObject(defaultSettings);
+	File.WriteAllText(configurationsPath, json);
+
+	// Log
+
+	return;
+}
